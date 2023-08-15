3007346911bbedfd097dcbf44a78f7e0d56d3a88
n0fr0st <krasnosheckov.v@yandex.ru>
2023-08-16 01:46:48 +0300

    new app 'news'

--- my_py_project/main/views.py
+++ my_py_project/main/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'main/index.html')
+    data = {
+        'title': 'Главная страница!!',
+        'values': ['some', 'hello', '123'],
+        'obj': {
+            'car': 'BMW',
+            'age': 18,
+            'hobby': 'Footbal'
+        }
+    }
+    return render(request, 'main/index.html', data)
 
 def about(request):
     return render(request, 'main/about.html')
