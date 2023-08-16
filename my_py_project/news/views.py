852d735a041571ed0f537182c8f14aace26508b9
n0fr0st <krasnosheckov.v@yandex.ru>
2023-08-16 21:37:26 +0300

    new patch

--- my_py_project/news/views.py
+++ my_py_project/news/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from . models import Articles
 
 def news_home(request):
-    news = Articles.objects.all()
+    news = Articles.objects.order_by('-date')
     return render(request, 'news/news_home.html', {'news': news})
