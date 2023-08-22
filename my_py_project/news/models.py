58782a08196473e8b0c8db853d6b6442b8966c17
n0fr0st <krasnosheckov.v@yandex.ru>
2023-08-22 07:47:04 +0300

    news post service

--- my_py_project/news/models.py
+++ my_py_project/news/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Articles(models.Model):
     title = models.CharField('Название', max_length=50)
-    anounce = models.CharField('Анонс', max_length=50)
+    anounce = models.CharField('Анонс', max_length=250)
     full_text = models.TextField('Статья')
     date = models.DateTimeField('Дата публикации')
 
